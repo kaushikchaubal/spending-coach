0c085347061adde829891ebc17baca751448a42c
Adrián González Martín <kaseyo23@gmail.com>
2015-02-07 12:41:05 +0000

    Added library folder

---
+++ spending-coach/tests/sc_tests.py
@@ -0,0 +1,12 @@
+import nose
+from unittest import TestCase
+
+from parser import Parser
+from reader import CsvReader
+
+class SpendingCoachTests(TestCase):
+
+    def test_parser(self):
+        reader = CsvReader('test.csv')
+        parser = Parser(reader)
+        print parser.parse()
