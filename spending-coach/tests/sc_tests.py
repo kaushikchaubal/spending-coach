8967e6411570199a416f320cd0eb718f218ee452
Adrián González Martín <kaseyo23@gmail.com>
2015-02-07 13:05:07 +0000

    reading test file now

--- spending-coach/tests/sc_tests.py
+++ spending-coach/tests/sc_tests.py
@@ -7,6 +7,6 @@ from reader import CsvReader
 class SpendingCoachTests(TestCase):
 
     def test_parser(self):
-        reader = CsvReader('samples/test.csv')
+        reader = CsvReader('samples/sample_statement.csv')
         parser = Parser(reader)
         print parser.parse()
