0c06bd78b9287ba0b3eabcd2e0ac0ddce886e5a8
Kaushik Chaubal <kaushikchaubal@gmail.com>
2015-02-07 13:25:17 +0000

    Remove unwanted backend implementations

--- resources/backend-rails-seed/Angle/app/controllers/api_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApiController < ActionController::Base
-  
-  def i18n
-    # locale param was defined in config/routes.rb
-    render file: "app/assets/i18n/" + params['locale']
-  end
-
-end
--- resources/backend-rails-seed/Angle/app/helpers/singleview_helper.rb
+++
@@ -1,2 +0,0 @@
-module SingleviewHelper
-end
