ae128ad69a1e1a932089fbadb2466d9f46e27a62
Ozo <ozoesono@gmail.com>
2015-02-07 16:05:07 +0000

    dsf

--- SpendingCoach/settings.py
+++ SpendingCoach/settings.py
@@ -30,22 +30,24 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    # 'django.contrib.admin',
-    # 'django.contrib.auth',
-    # 'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
-    # 'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'SpendingCoach'
 )
 
 MIDDLEWARE_CLASSES = (
-    # 'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    # 'django.contrib.messages.middleware.MessageMiddleware',
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'SpendingCoach.urls'
@@ -83,5 +85,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
-    './template/',
+    'templates/',
 )
--- SpendingCoach/urls.py
+++ SpendingCoach/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from views import index
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
@@ -7,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'SpendingCoach.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    (r'^$', TemplateView.as_view(template_name='index.html'))
+    # (r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^index', index)
 )
