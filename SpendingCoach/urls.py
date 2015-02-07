63f5a2ccb2c2a1ca359e66572d98d7e3d54ae170
Kaushik Chaubal <kaushikchaubal@gmail.com>
2015-02-07 14:04:20 +0000

    -m

--- SpendingCoach/urls.py
+++ SpendingCoach/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'SpendingCoach.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    (r'^$', TemplateView.as_view(template_name='index-hello.html')),
 )
