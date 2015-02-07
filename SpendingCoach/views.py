b2e8860f17b7b4c97bd534c702506ec96085fbc2
Adrián González Martín <kaseyo23@gmail.com>
2015-02-07 16:57:41 +0000

    Commit what we have till the presentation

--- SpendingCoach/views.py
+++ SpendingCoach/views.py
@@ -1,5 +1,33 @@
 from django.template.response import TemplateResponse
+import pprint 
 
+from services.parser import Parser
+from services.reader import CsvReader
 
 def index(request):
-    return TemplateResponse(request,  "dashboard.html", {})
+    
+    reader = CsvReader('SpendingCoach/sample.csv')
+    parser = Parser(reader)
+    
+    
+
+    # for index, data in parser:
+    #    results.append({})
+    pprint.PrettyPrinter()
+    pprint.pprint(parser.parse())
+
+    income = 0.00
+    expense = 0.00
+    
+    for p in parser.parse():
+        if p['is_expense'] is True:
+            expense += float(p['amount'])
+        else:
+            income += float(p['amount'])
+
+    result = {
+        "income" : income,
+        "expense" : expense
+    }
+    
+    return TemplateResponse(request,  "dashboard.html", result)
