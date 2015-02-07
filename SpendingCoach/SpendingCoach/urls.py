64bb0a18fd253cef5b5946030f8e78fb0aaacbcd
Kaushik Chaubal <kaushikchaubal@gmail.com>
2015-02-07 11:59:24 +0000

    Clean-up

--- SpendingCoach/SpendingCoach/urls.py
+++ SpendingCoach/SpendingCoach/urls.py
@@ -6,14 +6,5 @@ admin.autodiscover()
 from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'SpendingCoach.views.home', name='home'),
-    # url(r'^SpendingCoach/', include('SpendingCoach.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
     (r'^$', TemplateView.as_view(template_name='index.html')),
 )
